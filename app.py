74047c39dd297dbd9013b57b85d35f5d8fe5969d
lograjbhatt <bhattlograj@gmail.com>
2025-01-30 15:00:19 +0545

    more changes q

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app=Flask(__name__)
 
 def webout():
 
- return '<h1>DevOps is fun.test change one more time a</h1>'
+ return '<h1>DevOps is fun.test change one more time a</h1> <br> Dont mind'
 
 app.run(host='0.0.0.0',port=7000)
